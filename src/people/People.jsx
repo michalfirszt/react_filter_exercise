554a4b3a7a103651504e3ee1459d673a1f474d50
michalfirszt <michalfiszu@gmail.com>
2020-12-27 11:25:10 +0100

    Display people list

--- src/people/People.jsx
+++ src/people/People.jsx
@@ -7,22 +7,30 @@ import { bindActionCreators } from 'redux';
 
 class People extends Component {
   static propTypes = {
-    // ...
+    people: PropTypes.array
   };
 
-  // ...
+  peopleList = () => {
+    return this.props.people.map(person => {
+      return (
+        <div className='App-box' key={person.id}>
+          {person.name}
+        </div>
+      );
+    });
+  }
 
   render() {
     return (
       <div>
-        {/* ... */}
+        {this.peopleList()}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state) => ({
-  // ...
+  people: state.peopleReducer.people
 });
 
 const mapDispatchToProps = dispatch => bindActionCreators({
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-// ...
+import peopleReducer from './people';
 
 const rootReducer = combineReducers({
-  // ...
+  peopleReducer
 });
 
 export default rootReducer;
